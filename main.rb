0affa518c237fdbcfa5085d13d37c42a335ae1b0
Vladislav Korolyov <85311952+Vladislav122-kor@users.noreply.github.com>
2023-10-11 23:37:52 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,16 +1,23 @@
 require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/film'
+require_relative 'lib/product_collection'
 
 current_path = File.dirname(__FILE__)
-film = Film.from_file("#{current_path}/data/films/01.txt")
-book = Book.from_file("#{current_path}/data/books/01.txt")
 
-puts film
-puts book
+collection = ProductCollection.from_dir(current_path + '/data')
+puts collection.sort!(by: 'amount', order: 'asc').to_a
 
-begin
-  Product.from_file(current_path + '/data/films/01.txt')
-rescue NotImplementedError
-  puts 'Method for class Product.from_file is not implemented'
-end
+# collection.to_a.each { |product| puts product }
+
+# film = Film.from_file("#{current_path}/data/films/01.txt")
+# book = Book.from_file("#{current_path}/data/books/01.txt")
+
+# puts film
+# puts book
+
+# begin
+#   Product.from_file(current_path + '/data/films/01.txt')
+# rescue NotImplementedError
+#   puts 'Method for class Product.from_file is not implemented'
+# end
